db096c9362b2dedefb810610caae14db94898f79
fdemian <demian.fc@gmail.com>
2016-08-14 13:43:04 -0300

    Modificar Not Found.

--- src/components/Errors/NotFound.js
+++ src/components/Errors/NotFound.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 const NotFound = () => (
   <div>
-    <h3>404 page not found</h3>
-    <p>The page you are looking for does not exist.</p>
-	<p>Search this site</p>
-	<input type="search" />
+    <h3>404 Página no encontrada.</h3>
   </div>
 );
 
