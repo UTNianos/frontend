c4c11cbd916afbf2f20a7f81b6b57ed40bd8af96
fdemian <demian.fc@gmail.com>
2016-10-28 23:05:32 -0300

    Borrar css local y reemplazarlo por módulos de CSS.

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Footer from './Footer';
 import Navbar from '../Navbar/Container';
+import cssModules from 'react-css-modules';
+import styles from './Footer.scss';
+import Styles from './Base.scss';
 
 const App = ({children}) => (
-  <div>
+  <div styleName="App">
 	  <Navbar />
 	  {children}
 	  <Footer />
 	</div>
 );
 
-export default App;
+export default cssModules(App, styles, { allowMultiple: true });
