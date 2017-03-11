59b63a634a9edef2e5d10d47f2bffe1e49c133dd
fdemian <demian.fc@gmail.com>
2017-03-10 23:31:13 -0300

    Actualizar paquetes viejos. Agregar snapshot del test de la barra de navegación.

--- src/Errors/NotFound.js
+++ src/Errors/NotFound.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const NotFound = () => (
   <div>
-    <h3>404 Página no encontrada.</h3>
+    <h1>404</h1>
+	<h3>Página no encontrada. Deal with it.</h3>	
   </div>
 );
 
