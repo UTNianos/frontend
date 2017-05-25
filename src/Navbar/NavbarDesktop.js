47d8d7a593b0f181d86871b703bc404f2afdeffb
fdemian <demian.fc@gmail.com>
2017-05-25 14:34:12 -0300

    Agregar Navbars para desktop y mobile.

---
+++ src/Navbar/NavbarDesktop.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { NavLink as Link } from 'react-router-dom';
+import { Menu, Row, Col } from 'antd';
+import LoginMenu from './LoginMenu/LoginMenu';
+import './Navbar.css';
+//import AccountMenu from './AccountMenu/AccountMenu';
+import logo from './logo_utnianos.png'; // Tell Webpack this JS file uses this image
+
+const imgStyle = {
+  marginLeft: '17px',
+  marginTop: '9px',
+  width: '150px',
+  height: '30px'
+};
+
+
+const NavbarDesktop = () => {
+
+  return (
+   <Menu mode="horizontal">
+    <Row type="flex" justify="space-between">
+	    <Col span={4} style={{paddingLeft:'80px'}}>
+       <span>
+          <Link to="/" activeClassName="HomeLinkActive">
+            <img src={logo} alt="UTNianos" style={imgStyle} />
+          </Link>
+       </span>
+      </Col>
+      <Col span={4}></Col>
+      <Col span={4}></Col>
+	    <Col span={4}></Col>
+      <Col span={4}></Col>
+	    <Col span={4}></Col>
+	  </Row>
+  </Menu>
+  )
+};
+
+export default NavbarDesktop;
---
+++ src/Navbar/NavbarMobile.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import { Menu, Row, Col, Icon, Button } from 'antd';
+import logo from './logo_utnianos.png';
+
+const imgStyle = {
+  marginLeft: '17px',
+  marginTop: '9px',
+  width: '150px',
+  height: '25px'
+};
+
+const NavbarMobile = () => {
+
+  return (
+   <Menu mode="horizontal">
+    <Row type="flex" justify="space-between">
+	  <Col span={4}></Col>
+      <Col span={4}>
+	  	<Link to="/">
+		  <img src={logo} alt="ReactiveBB" style={imgStyle} />
+		</Link>
+	  </Col>
+      <Col span={4}></Col>
+      <Col span={4}></Col>
+	  <Col span={4}></Col>
+	  <Col span={4}>
+		<Button>
+			<Icon type="bars" />
+		</Button>
+	  </Col>
+	 </Row>
+    </Menu>
+  )
+}
+
+export default NavbarMobile;
