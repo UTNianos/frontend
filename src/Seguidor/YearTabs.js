7d5128ae5e7cb737695962ebf739b20b90bbca9e
fdemian <demian.fc@gmail.com>
2017-06-11 20:34:42 -0300

    No hardcodear el numero de años para cada tab.

--- src/Seguidor/YearTabs.js
+++ src/Seguidor/YearTabs.js
@@ -6,49 +6,50 @@ const TabPane = Tabs.TabPane;
 
 const yearsPerTab = 3;
 
-function callback(key) {
-  //console.log(key);
-}
+function callback(key) {}
 
 function getYearsArray(yearsPerTab, subjects) {
 
-  let i = 0;
+  let index = 0;
+  let yearIndex = 1;
   let yearsArray = [];
   let tabsArray = [];
-  let currentYear = [];
+  let currentYear = 0;
 
-  while(currentYear.length !== 0 || i === 0) {
-    const limit = yearsPerTab +i;
+  const maxYearMaterias = 5;
 
-    for(; i<= limit; i++) {
-      currentYear = subjects.filter(m => m.year == i.toString());
+  while(yearIndex <= maxYearMaterias) {
 
-      if(currentYear.length !== 0)
-         yearsArray.push(currentYear);
-    }
-    tabsArray.push(yearsArray);
+     index = 1;
+     while(index <= yearsPerTab && yearIndex <= maxYearMaterias) {
+
+        currentYear = {
+            year: yearIndex,
+            subjects: subjects.filter(m => m.year == yearIndex)
+        };
+
+        yearsArray.push(currentYear);
+
+        index++;
+        yearIndex++
+     }
+
+     tabsArray.push(yearsArray);
+     yearsArray = [];
+
   }
 
   return tabsArray;
+
 }
 
 const YearTabs = ({materias, updateFn}) => {
 
-  //const studyYears = getYearsArray(yearsPerTab, materias);
-
-  const yearOne = { year: 1, subjects: materias.filter(m => m.year == 1) };
-  const yearTwo = { year: 2, subjects: materias.filter(m => m.year == 2) };
-  const yearThree = { year: 3, subjects:  materias.filter(m => m.year == 3) };
-  const yearFour = { year: 4, subjects:  materias.filter(m => m.year == 4) };
-  const yearFive = { year: 5, subjects:  materias.filter(m => m.year == 5) };
-  const tabOne = [yearOne, yearTwo, yearThree];
-  const tabTwo = [yearFour, yearFive];
-
-  const yearsByTab = [tabOne, tabTwo];
+  const studyYears = getYearsArray(yearsPerTab, materias);
 
   return(
   <Tabs onChange={callback} type="card">
-    {yearsByTab.map((tabYears, i) =>
+    {studyYears.map((tabYears, i) =>
       <TabPane tab={(i+1).toString()} key={(i+1).toString()}>
         <SubjectYears years={tabYears} updateFn={updateFn} />
       </TabPane>
