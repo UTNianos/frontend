9a4fd469f1d53fefe4225b482fc62f2b15926805
fdemian <demian.fc@gmail.com>
2017-06-30 21:57:27 -0300

    Fixes para el linter. Restaurar no-loop-func.

--- src/Seguidor/Carousel/Carousel.js
+++ src/Seguidor/Carousel/Carousel.js
@@ -7,74 +7,72 @@ import './Carousel.css';
 
 class Carousel extends Component {
 
- constructor(props) {
+  constructor(props) {
     super(props);
-	const { materias } = props;	
-	this.state = {
-	   currentTab: 1,
-	   totalTabs: 0,
-	   studyYears: []
-	};
- }
- 
- componentWillReceiveProps(nextProps) {
-	
-	const {materias, yearsPerTab} = nextProps;	
-	const studyYears = getYearsArray(yearsPerTab, materias);
-	
-	this.setState({ 
-	  totalTabs: studyYears.length,
-	  studyYears: studyYears
-	});
- }
-  
- nextTab() {
-   if(this.state.currentTab < this.state.totalTabs) {
-	 this.setState({
-	    currentTab: this.state.currentTab+1
-     });
-   }
- }
+    this.state = {
+      currentTab: 1,
+      totalTabs: 0,
+      studyYears: []
+    };
+  }
 
- prevTab() {
-   if(this.state.currentTab > 1){
-	 this.setState({
-	   currentTab: this.state.currentTab-1
-	 });
-   }
- }
- 
- render() {
-	
-	if(this.state.studyYears.length <= 0)
-		return null;
-	
-	const index = this.state.currentTab-1;
-	const years = this.state.studyYears[index];
-	const {updateFn} = this.props;
-		
+  componentWillReceiveProps(nextProps) {
+
+    const { materias, yearsPerTab } = nextProps;
+    const studyYears = getYearsArray(yearsPerTab, materias);
+
+    this.setState({
+      totalTabs: studyYears.length,
+      studyYears
+    });
+  }
+
+  nextTab() {
+    if (this.state.currentTab < this.state.totalTabs) {
+      this.setState({
+        currentTab: this.state.currentTab + 1
+      });
+    }
+  }
+
+  prevTab() {
+    if (this.state.currentTab > 1) {
+      this.setState({
+        currentTab: this.state.currentTab - 1
+      });
+    }
+  }
+
+  render() {
+
+    if (this.state.studyYears.length <= 0) { return null; }
+
+    const index = this.state.currentTab - 1;
+    const years = this.state.studyYears[index];
+    const { updateFn } = this.props;
+
     return (
-    <div>
-	  <CarouselArrows 
-		prevFn={this.prevTab.bind(this)} 
-		nextFn={this.nextTab.bind(this)} 
-	  />
-	  <CSSTransitionGroup
-		transitionName="TransitionGroup"
-		transitionAppear={false}		
-		transitionEnterTimeout={1000}
-        transitionLeaveTimeout={1000}
-		transitionEnter={true}
-		transitionLeave={true}
-	   >
-	    <SubjectYears 
-		  years={years}
-		  updateFn={updateFn}
-		/>
-      </CSSTransitionGroup>      
-    </div>
+      <div>
+        <CarouselArrows
+          prevFn={this.prevTab.bind(this)}
+          nextFn={this.nextTab.bind(this)}
+        />
+        <CSSTransitionGroup
+          transitionName="TransitionGroup"
+          transitionAppear={false}
+          transitionEnterTimeout={1000}
+          transitionLeaveTimeout={1000}
+          transitionEnter
+          transitionLeave
+        >
+          <SubjectYears
+            years={years}
+            updateFn={updateFn}
+          />
+        </CSSTransitionGroup>
+      </div>
     );
- }
+  }
 }
 
 export default Carousel;
