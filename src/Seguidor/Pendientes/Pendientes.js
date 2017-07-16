0231c4b761aeeb73e89663248140c73797a2f1b4
fdemian <demian.fc@gmail.com>
2017-07-15 22:57:06 -0300

    Cambios estéticos a las materias pendientes.

--- src/Seguidor/Pendientes/Pendientes.js
+++ src/Seguidor/Pendientes/Pendientes.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
+const DisplayList = ({title, list}) => 
+{
+   if(list.length === 0)
+      return null;
+    
+   return(
+   <div>
+   
+	 <p>{title}</p>
+	
+     <ul>
+	 {list.map(pendiente =>
+	  <li key={pendiente.id}>
+		{pendiente.name}
+	  </li>
+	  )}
+	 </ul>
+	
+   </div>
+   );  		
+}
+
 const Pendientes = ({pendientes, materias}) => {
  
  const firma = materias.filter(m => pendientes.firma.includes(m.id)); 
@@ -7,14 +29,16 @@ const Pendientes = ({pendientes, materias}) => {
   
  return(
  <div>
-    <p>Para cursar te faltan las siguientes correlativas:</p>
-	<ul>
-	 {firma.map(pendiente =>
-	  <li key={pendiente.id}>
-		{pendiente.name}
-	  </li>
-	  )}
-	</ul>
+    <strong>Para cursar te faltan las siguientes correlativas:</strong>	
+		
+    <div>
+       <DisplayList title="Con cursada: " list={firma} />
+    </div>
+	
+    <div>
+       <DisplayList title="Con final: " list={final} />
+    </div>	
+	
   </div>
  );		
 }
