946bf7b4e0382c86dc049dabe69fce13d184dfb8
DOMINIO\fcaminiti <fcaminiti@gyfsi.com.ar>
2017-07-18 11:35:01 -0300

    Hacer feliz al linter.

--- src/Seguidor/Pendientes/Container.js
+++ src/Seguidor/Pendientes/Container.js
@@ -1,16 +1,13 @@
 import { connect } from 'react-redux';
 import Pendientes from './Pendientes';
-//import getMateriasEstado from './Selector';
+// import getMateriasEstado from './Selector';
 
 const mapStateToProps = state => ({
   materias: state.seguidor.materias
 });
 
-const mapDispatchToProps = dispatch => ({});
-
 const PendientesContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
+  mapStateToProps
 )(Pendientes);
 
 export default PendientesContainer;
