59e8a58a070cebe4df670d58f11218165b42a9a4
fdemian <demian.fc@gmail.com>
2017-10-08 22:29:49 -0300

    Separar vista de carousel en su propia carpeta.

--- src/Seguidor/Seguidor.jsx
+++ src/Seguidor/Seguidor.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import MediaQuery from 'react-responsive'
 import FetchingIndicator from '../Fetching/FetchingIndicator'
 import SeguidorHeading from './SeguidorHeading'
-import Carousel from './Carousel/Carousel'
+import CarouselView from './CarouselView/CarouselView';
 
 class Seguidor extends Component {
   componentDidMount() {
@@ -11,11 +10,17 @@ class Seguidor extends Component {
 
   render() {
 
-    const { isFetching, error, materias } = this.props
+    const { isFetching, error, materias, updateEstado } = this.props;
 
-    if (isFetching) { return <FetchingIndicator /> }
+    if (isFetching)
+      return <FetchingIndicator />;
 
-    if (error) { return <p>Hubo un error recuperando las materias</p> }
+    if (error)
+      return (
+      <p>
+        Hubo un error recuperando las materias.
+      </p>
+      );
 
     return (
       <div>
@@ -24,22 +29,8 @@ class Seguidor extends Component {
           <SeguidorHeading />
         </div>
 
-        <div>
-          <MediaQuery minDeviceWidth={1224}>
-            <Carousel
-              materias={materias}
-              yearsPerTab={3}
-              updateFn={this.props.updateEstado}
-            />
-          </MediaQuery>
-          <MediaQuery maxDeviceWidth={1224}>
-            <Carousel
-              materias={materias}
-              yearsPerTab={1}
-              updateFn={this.props.updateEstado}
-            />
-          </MediaQuery>
-        </div>
+        <CarouselView materias={materias} updateEstado={updateEstado} />
+
       </div>
     )
 
