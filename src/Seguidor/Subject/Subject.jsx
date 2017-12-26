41f412281870faa078b0e0756cc88c4f8975493d
DOMINIO\fcaminiti <fcaminiti@gyfsi.com.ar>
2017-12-26 15:57:48 -0300

    Cambiar tabs por espacios en Subject.jsx. Apacigüar a linter!jarry (eliminar espacio insertado con anterioridad / reinsertar ignores del gitignore).

--- src/Seguidor/Subject/Subject.jsx
+++ src/Seguidor/Subject/Subject.jsx
@@ -3,10 +3,10 @@ import './Subject.css';
 import { Popover, Card, Menu, Dropdown } from 'antd';
 
 const subjectValues = [
-	{"number": 1, "name": "Pendiente"},
-	{"number": 2, "name": "Cursando"},
-	{"number": 3, "name": "Firmada"},
-	{"number": 4, "name": "Aprobada"}
+    {"number": 1, "name": "Pendiente"},
+    {"number": 2, "name": "Cursando"},
+    {"number": 3, "name": "Firmada"},
+    {"number": 4, "name": "Aprobada"}
 ];
 
 function updateMateria(e, id, updateFn){
@@ -33,45 +33,45 @@ const Subject = ({id, name, status, cursada, final, updateEstado}) => {
   const subjectClass = "Subject " + statusName;
   
   const menu = (
-	<Menu onClick={(e) => updateMateria(e, id, updateEstado)}>
-	  {subjectValues.map((sValue, i) =>
-	   <Menu.Item key={i} >
+    <Menu onClick={(e) => updateMateria(e, id, updateEstado)}>
+      {subjectValues.map((sValue, i) =>
+       <Menu.Item key={i} >
           {sValue.name}
-	   </Menu.Item>
-	   )}
-	</Menu>
+       </Menu.Item>
+       )}
+    </Menu>
   );
 
   if(cursada) {
-	  return (
-	  <div className={subjectClass}>
-		<Card className="SubjectCard">
-		  <strong className="SubjectName">{name}</strong>
-		  <Dropdown.Button overlay={menu} style={{'display': 'inline', 'marginLeft': '4px'}}>
-		   {statusName}
-		  </Dropdown.Button>
-		</ Card>
-	  </div>
+      return (
+      <div className={subjectClass}>
+        <Card className="SubjectCard">
+          <strong className="SubjectName">{name}</strong>
+          <Dropdown.Button overlay={menu} style={{'display': 'inline', 'marginLeft': '4px'}}>
+           {statusName}
+          </Dropdown.Button>
+        </ Card>
+      </div>
     );
   }
 
   if(!cursada) {
-	 return (
-	 <div className="Subject Disabled">
-	    <Popover content={PopoverContent} title={name} trigger="hover">
-		  <Card className="SubjectCard">
-		    <strong className="SubjectName">{name}</strong>
-		    <Dropdown.Button  
-			       disabled 
-				   style={{'display': 'inline', 'marginLeft': '4px'}} 
-				   overlay={menu}
-			>
-			Pendiente
-		    </Dropdown.Button>
-		  </Card>
-		</ Popover>
-	 </div>
-	 );
+     return (
+     <div className="Subject Disabled">
+        <Popover content={PopoverContent} title={name} trigger="hover">
+          <Card className="SubjectCard">
+            <strong className="SubjectName">{name}</strong>
+            <Dropdown.Button  
+                   disabled 
+                   style={{'display': 'inline', 'marginLeft': '4px'}} 
+                   overlay={menu}
+            >
+           Pendiente
+            </Dropdown.Button>
+          </Card>
+        </ Popover>
+     </div>
+     );
   }
 
 }
