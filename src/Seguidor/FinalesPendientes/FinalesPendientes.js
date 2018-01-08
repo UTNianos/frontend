e5636d1b48a1b8ce70683ff9c494b7a780e72b6b
fdemian <demian.fc@gmail.com>
2018-01-07 21:48:05 -0300

    Filtrar correctamente las materias pendientes de final.

--- src/Seguidor/FinalesPendientes/FinalesPendientes.js
+++ src/Seguidor/FinalesPendientes/FinalesPendientes.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { List } from 'antd';
+import './FinalesPendientes.css';
 
 const FinalesPendientes = ({materias, updateEstado}) => {
 
-  const pendientesDeFinal = materias.filter(m => m.status === 3);
+  const pendientesDeFinal = materias.filter(m => m.final === true && m.status == 3);
 
   return(
-  <div>
-    {pendientesDeFinal.map(mat =><p>{mat.name}</p>)}
+  <div className="PendientesList">
+  
+    <List
+    itemLayout="horizontal"
+    dataSource={pendientesDeFinal}
+    renderItem={item => (
+      <List.Item actions={[<a>Aprobada</a>]}>
+        <List.Item.Meta
+          title={<a href="https://ant.design">{item.name}</a>}/>
+      </List.Item>
+		)}
+	/>  
   </div>
   );
 
