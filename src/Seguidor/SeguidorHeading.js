d3dcd2bd72f64e7dc6f9e8652484a570fc490535
fdemian <demian.fc@gmail.com>
2018-02-18 23:19:09 -0300

    Eliminar botones para elegir materias y reemplazarlos por un dropdown.

--- src/Seguidor/SeguidorHeading.js
+++ src/Seguidor/SeguidorHeading.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import {
-  faClock,
-  faTree,
-  faArrowsAltH
-} from '@fortawesome/fontawesome-free-solid';
-import { Button } from 'antd';
+import { Select } from 'antd';
 import './Seguidor.css';
 
-const SeguidorHeading = ({ changeViewType /* , currentView */ }) => (
+const Option = Select.Option;
+
+const SeguidorHeading = ({ changeViewType , currentView }) => (
   <div>
 
     <div className="SeguidorTitle">
@@ -16,15 +12,21 @@ const SeguidorHeading = ({ changeViewType /* , currentView */ }) => (
     </div>
 
     <div className="ViewChooser">
-      <Button onClick={() => changeViewType('carousel')} >
-        <FontAwesomeIcon icon={faArrowsAltH} size="lg" />
-      </Button>
-      <Button onClick={() => changeViewType('tree')}>
-        <FontAwesomeIcon icon={faTree} size="lg" />
-      </Button>
-      <Button onClick={() => changeViewType('finales')}>
-        <FontAwesomeIcon icon={faClock} size="lg" />
-      </Button>
+
+      <Select
+        showSearch
+        style={{ width: 200 }}
+        placeholder="Elegir una vista"
+        optionFilterProp="children"
+        onChange={(value) => changeViewType(value)}
+        defaultValue={currentView}
+        filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+     >
+        <Option value="carousel">Vista alargada</Option>
+        <Option value="tree">Vista compacta</Option>
+        <Option value="finales">Finales Pendientes</Option>
+     </Select>
+
     </div>
 
   </div>
