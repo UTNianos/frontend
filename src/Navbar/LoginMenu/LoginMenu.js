34c1891104341b906ed7aecf983af83000f49f0c
Federico Caminiti <fdemian@MacBook-Pro-de-Federico.local>
2018-09-16 01:13:08 -0300

    Upgrade old packages.

--- src/Navbar/LoginMenu/LoginMenu.js
+++ src/Navbar/LoginMenu/LoginMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from 'antd';
 import { Link } from 'react-router-dom';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faSignInAlt,
   faUserPlus
