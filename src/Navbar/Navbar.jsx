821dd1d3f4f9d18d1903712d610671c223384d84
fdemian <demian.fc@gmail.com>
2019-03-24 16:47:13 -0300

    Actualizar paquetes a la última versión.

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import NavbarDesktop from './NavbarDesktop';
 import NavbarMobile from './NavbarMobile';
 import MediaQuery from 'react-responsive';
